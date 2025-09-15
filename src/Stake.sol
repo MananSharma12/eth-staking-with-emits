0cefab5501de10a129648cb31242259c6e5bb7d0
MananSharma12 <manan.sharma1205@gmail.com>
2025-09-15 17:28:16 +0530

    Add reward tracking and claiming mechanisms to `Stake` contract

--- src/Stake.sol
+++ src/Stake.sol
@@ -3,21 +3,44 @@ pragma solidity ^0.8.13;
 
 contract Stake {
     mapping(address => uint256) public balances;
+    mapping(address => uint256) public lastUpdateTime;
+    mapping(address => uint256) public unclaimedRewards;
 
     constructor() {
     }
 
     function stake() public payable {
         require(msg.value > 0, "You have to pay!");
+        if (!lastUpdateTime) {
+            lastUpdateTime[msg.sender] = block.timestamp;
+        } else {
+            unclaimedRewards[msg.sender] = (block.timestamp - lastUpdateTime[msg.sender]) * balances[msg.sender];
+            lastUpdateTime[msg.sender] = block.timestamp;
+        }
+
         balances[msg.sender] += msg.value;
     }
 
     function unstake(uint256 _amount) public {
         require(_amount <= balances[msg.sender], "You don't have enough balance to unstake");
+
+        unclaimedRewards[msg.sender] = (block.timestamp - lastUpdateTime[msg.sender]) * balances[msg.sender];
+        lastUpdateTime[msg.sender] = block.timestamp;
+
+        payable(msg.sender).transfer(_amount);
         balances[msg.sender] -= _amount;
     }
 
-    function getRewards() public {}
+    function getRewards(address _address) public {
+        return unclaimedRewards[_address] + ((block.timestamp - lastUpdateTime[_address]) * balances[_address]);
+    }
 
-    function claimRewards() public {}
+    function claimRewards() public {
+        unclaimedRewards[msg.sender] += (block.timestamp - lastUpdateTime[msg.sender]) * balances[msg.sender];
+
+        // Transfer all unclaimed rewards to the user
+
+        unclaimedRewards[msg.sender] = 0;
+        lastUpdateTime[msg.sender] = block.timestamp;
+    }
 }
